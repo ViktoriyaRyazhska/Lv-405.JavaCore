3b8889dda3ab8f32985efb8a7827777df201a57d
Yaroslav10 <yaroslaw1910k@gmail.com>
2019-04-25 10:13:55 +0300

    First HW

--- src/program/Home.java
+++ src/program/Home.java
@@ -10,13 +10,13 @@ public class Home {
 
 		System.out.println("Enter the radius");
 		double radius = Double.parseDouble(br.readLine());
-
+//2
 		double Area = (double) 3.14 * radius * radius;
 		double Perimeter = (double) 2 * 3.14 * radius;
 
 		System.out.println(Area);
 		System.out.println(Perimeter);
-
+//2
 		System.out.println("What is your name?");
 		String name = br.readLine();
 		System.out.println("Where do you live, " + name + "?");
@@ -26,29 +26,29 @@ public class Home {
 		
 		
 	
+	//3	
 		
-		
-		System.out.println("яка варт≥сть першого дзв≥нка?");
+		System.out.println("Enter the coast fot the Ukranian calls?");
 		double value1 = Double.parseDouble(br.readLine());
-		System.out.println("яка тривал≥сть першого дзв≥нка?");
+		System.out.println("Enter the duration the Ukranian call?");
 		double time1 = Double.parseDouble(br.readLine());
 		double cost1 =  value1 * time1;
-		System.out.println("¬арт≥сть €ка ст€гнетьс€ за перший зв≥нок " + cost1);
-		System.out.println("яка варт≥сть другого дзв≥нка?");
+		System.out.println("The cost of the first call " + cost1);
+		System.out.println("Enter the coast fot the American calls?");
 		double value2 = Double.parseDouble(br.readLine());
-		System.out.println("яка тривал≥сть другого дзв≥нка?");
+		System.out.println("Enter the duration the American call?");
 		double time2 = Double.parseDouble(br.readLine());
 		double cost2 =  value2 * time2;
-		System.out.println("¬арт≥сть €ка ст€гнетьс€ за другий зв≥нок " + value2 * time2);
-		System.out.println("яка варт≥сть третього дзв≥нка?");
+		System.out.println("The cost of the second call " + value2 * time2);
+		System.out.println("Enter the coast fot the Poland calls?");
 		double value3 = Double.parseDouble(br.readLine());
-		System.out.println("яка тривал≥сть третього дзв≥нка?");
+		System.out.println("Enter the duration the Poland call?");
 		double time3 = Double.parseDouble(br.readLine());
 		double cost3 =  value3 * time3;
-		System.out.println("¬арт≥сть €ка ст€гнетьс€ за трет≥й зв≥нок " + value3 * time3);
+		System.out.println("The cost of the third call " + value3 * time3);
 		
 		
-		System.out.println("«агальна варт≥сть ус≥х дзв≥нк≥в " + (cost1+cost2+cost3));
+		System.out.println("The cost of all calls " + (cost1+cost2+cost3));
 
 	}
 }
