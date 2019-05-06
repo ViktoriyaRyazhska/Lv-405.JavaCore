e3f75aa0285d58e750e68b01a157714cd0b1048b
Yaroslav Yatsyuk <yaroslav.yatsyuk1@gmail.com>
2019-05-07 00:03:54 +0300

    Updated exception handler process

--- src/main/java/HomeWorkOOPLesson3/Person.java
+++ src/main/java/HomeWorkOOPLesson3/Person.java
@@ -27,34 +27,44 @@ public class Person {
         return birthdayYear;
     }
 
-    public int age(){
-        if(this.birthdayYear.equals("unknown")){
+    /**
+     * @return  person's age in integer format or NumberFormatException;
+     * if birthdayYear contains non digit characters;
+     * @throws NumberFormatException  if birthdayYear contains non digit characters;
+     */
+    public int age() throws NumberFormatException{
+
+        String birthdayYear = this.getBirthdayYear();
+
+        if(birthdayYear.equals("unknown")){
             return -1;
         }
-        try {
 
-            return LocalDate.now().getYear() - Integer.parseInt(getBirthdayYear());
-
-        } catch (NumberFormatException e) {
-            System.out.println("Incorrect birthday year, you should use only digits");
-            return -1;
+        for (char c : birthdayYear.toCharArray()) {
+            if(!Character.isDigit(c)){
+                throw new NumberFormatException("You cannot use non digit character for birthday year");
+            }
         }
 
+        return LocalDate.now().getYear() - Integer.parseInt(birthdayYear);
     }
 
-    public boolean input(String name, String birthdayYear){
-        try {
-            Integer.valueOf(birthdayYear);
+    /**
+     * This method update information about Person;
+     * @param name person name;
+     * @param birthdayYear  birthday year in String format;
+     * @throws NumberFormatException  if birthdayYear contains non digit characters;
+     */
+    public void input(String name, String birthdayYear) throws NumberFormatException{
 
-            this.name = name;
-            this.birthdayYear = birthdayYear;
-
-        } catch (NumberFormatException e) {
-            System.out.println("Incorrect birthday year, you should use only digits");
-            return false;
+        for (char c : birthdayYear.toCharArray()) {
+             if(!Character.isDigit(c)){
+                 throw new NumberFormatException("You cannot use non digit character for birthday year");
+             }
         }
 
-        return true;
+        this.name = name;
+        this.birthdayYear = birthdayYear;
     }
 
     public void changeName(String newName){
@@ -65,24 +75,34 @@ public class Person {
         return "Name = " + getName() + "\n" + "Age = " + this.age();
     }
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        try(BufferedReader consoleReader = new BufferedReader(new InputStreamReader(System.in))) {
 
-        Person[] persons = new Person[5];
+            Person[] persons = new Person[5];
 
-        for (int index = 0; index < persons.length; index++) {
-            System.out.print("Enter the name = ");
-            String personName = consoleReader.readLine();
-            System.out.print("Enter the birthday year = ");
-            String personBirthdayYear = consoleReader.readLine();
-            persons[index] = new Person(personName, personBirthdayYear);
+            for (int index = 0; index < persons.length; index++) {
+
+                System.out.print("Enter the name = ");
+                String personName = consoleReader.readLine();
+                System.out.print("Enter the birthday year = ");
+                String personBirthdayYear = consoleReader.readLine();
+
+                Person person = new Person();
+                person.input(personName, personBirthdayYear);
+                persons[index] = person;
+
+            }
+
+            System.out.println(persons[0].output());
+            System.out.println(persons[1].output());
+            System.out.println(persons[2].output());
+            System.out.println(persons[3].output());
+            System.out.println(persons[4].output());
+
+        }catch (IOException | NumberFormatException e ){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
-
-        System.out.println(persons[0].output());
-        System.out.println(persons[1].output());
-        System.out.println(persons[2].output());
-        System.out.println(persons[3].output());
-        System.out.println(persons[4].output());
 
     }
 
