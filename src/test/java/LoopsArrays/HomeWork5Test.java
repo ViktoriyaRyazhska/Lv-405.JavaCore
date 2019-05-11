320fe3d7edf978cc6e587ae1ef5fdb97adcb799d
Yaroslav Yatsyuk <yaroslav.yatsyuk1@gmail.com>
2019-05-11 17:16:40 +0300

    Fix incorrect test

--- src/test/java/LoopsArrays/HomeWork5Test.java
+++ src/test/java/LoopsArrays/HomeWork5Test.java
@@ -5,32 +5,41 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class HomeWork5Test {
+    private static int[] arr = {2,5,1,3,6,-1,-2,2,2,2};
 
     @Test
-    public void getMinElemAndItPosition() {
-        int[] arr1 = {2,5,1,3,6,-1,-2,2,2,2};
-        int[] result = {-2,6};
-        assertArrayEquals(result,HomeWork5.getMinElemAndItPosition(arr1));
+    public void getMinElemPosition() {
+        assertEquals(6 ,HomeWork5.getMinElemPosition(arr));
     }
 
     @Test
     public void positionOfSecondPositiveNumber() {
-        int[] arr1 = {2,5,1,3,6,-1,-2,2,2,2};
-        assertEquals(1,HomeWork5.positionOfSecondPositiveNumber(arr1));
+        assertEquals(1,HomeWork5.positionOfSecondPositiveNumber(arr));
     }
 
     @Test
-    public void getDaysArray() {
-        assertEquals(29,HomeWork5.getDaysArray(2,true).length);
-        assertEquals(28,HomeWork5.getDaysArray(2,false).length);
+    public void getDaysArrayShouldBeNull() {
         assertNull(HomeWork5.getDaysArray(13,true));
     }
 
     @Test
-    public void calculateSumOrProductOfNumbersArr() {
-        int[] arr1 = {2,5,1,3,6,-1,-2,2,2,2};
-        assertEquals(17,HomeWork5.calculateSumOrProductOfNumbersArr(arr1));
-        int[] arr2 = {2,5,-1,3,6,-1,-2,2,2,2};
-        assertEquals(16,HomeWork5.calculateSumOrProductOfNumbersArr(arr2));
+    public void getDaysArray() {
+        assertArrayEquals(new int[31],HomeWork5.getDaysArray(5,true));
+    }
+
+    @Test
+    public void calculateSumNumbers() {
+        assertEquals(16,HomeWork5.calculateSumOrProductOfNumbersArr(arr));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllArgExcept() {
+        HomeWork5.showCarsWithCertainModelYear(null, 2000);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void shouldThrowNullPointerExcept() {
+        Car[] cars = {null};
+        HomeWork5.showCarsWithCertainModelYear(cars, 2000);
     }
 }
